823fa0a633b4acfda69b308d8173b5aea590896c
JaroslawZielinski <jarek.zielinski82@gmail.com>
2019-01-24 18:12:57 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir -p dockers
+mkdir -p run
+cp -r vendor/jaroslawzielinski/project-dockerized/dockers
+cp -r vendor/jaroslawzielinski/project-dockerized/run .
+chmod +x run/dockerized
+cp vendor/jaroslawzielinski/project-dockerized/.env .
+cp vendor/jaroslawzielinski/project-dockerized/docker-compose.yml .
