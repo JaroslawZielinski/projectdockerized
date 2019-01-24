94072d67cf5820ee23305400beafa7fcaf9fd0b8
JaroslawZielinski <jarek.zielinski82@gmail.com>
2019-01-24 22:27:06 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,24 +8,32 @@ if [ ! -f "dockers/mysql/Dockerfile" ];
 then
   echo "\"dockers/mysql/Dockerfile\" copied."
   cp vendor/jaroslawzielinski/project-dockerized/dockers/mysql/Dockerfile dockers/mysql
+else
+  echo "\"dockers/mysql/Dockerfile\" already exists."
 fi
 
 if [ ! -f "dockers/mysql/mysqld.cnf" ];
 then
   echo "\"dockers/mysql/mysqld.cnf\" copied."
   cp vendor/jaroslawzielinski/project-dockerized/dockers/mysql/mysqld.cnf dockers/mysql
+else
+  echo "\"dockers/mysql/mysqld.cnf\" already exists."
 fi
 
 if [ ! -f "dockers/php/Dockerfile" ];
 then
   echo "\"dockers/php/Dockerfile\" copied."
   cp vendor/jaroslawzielinski/project-dockerized/dockers/php/Dockerfile dockers/php
+else
+  echo "\"dockers/php/Dockerfile\" already exists."
 fi
 
 if [ ! -f "dockers/php/php.ini" ];
 then
   echo "\"dockers/php/php.ini\" copied."
   cp vendor/jaroslawzielinski/project-dockerized/dockers/php/php.ini dockers/php
+else
+  echo "\"dockers/php/php.ini\" already exists."
 fi
 
 if [ ! -f "run/dockerized" ];
@@ -35,16 +43,22 @@ then
 
   echo "\"run/dockerized\" is now executable."
   chmod +x run/dockerized
+else
+  echo "\"run/dockerized\" already exists."
 fi
 
 if [ ! -f ".env" ];
 then
   echo "\"dockers/mysql/Dockerfile\" copied."
   cp vendor/jaroslawzielinski/project-dockerized/.env .
+else
+  echo "\"dockers/mysql/Dockerfile\" already exists."
 fi
 
 if [ ! -f "docker-compose.yml" ];
 then
   echo "\"dockers/mysql/Dockerfile\" copied."
   cp vendor/jaroslawzielinski/project-dockerized/docker-compose.yml .
+else
+  echo "\"dockers/mysql/Dockerfile\" already exists."
 fi
